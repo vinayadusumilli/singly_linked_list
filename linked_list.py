3f39baf63f1ec16048e6e52cb3f617145fb2da4d
Vinay Adusumilli <146542274+vinayadusumilli@users.noreply.github.com>
2024-09-24 20:17:38 +0100

    Added the functionality that add node to end of the LinkedList

--- linked_list.py
+++ linked_list.py
@@ -13,9 +13,21 @@ class LinkedList:
             while n is not None:
                 print(n.data)
                 n = n.ref
-                print(n)
 
     def add_begin_node(self, data):
         new_node = Node(data)
         new_node.ref = self.head
         self.head = new_node
+
+    def add_end_node(self, data):
+        new_node = Node(data)
+        n = self.head
+        if n is None:
+            new_node.ref = new_node
+            self.head = new_node
+        else:
+            before_node = None
+            while n is not None:
+                before_node = n
+                n = n.ref
+            before_node.ref = new_node
--- main.py
+++ main.py
@@ -3,5 +3,8 @@ from linked_list import LinkedList
 singly_list = LinkedList()
 singly_list.add_begin_node(20)
 singly_list.add_begin_node(10)
+singly_list.add_end_node(40)
+singly_list.add_begin_node(0)
+singly_list.add_end_node(906)
 singly_list.print_data()
 
